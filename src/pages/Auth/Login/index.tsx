1fa030303e92a8eae4f1b0d66405dbefc3853f11
lowfnm <artem.melinevskiy21@gmail.com>
2025-09-11 07:48:39 +0300

    fix: test

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/components/ui/Button';
-import { Input } from  '@/components/ui/Input';
+import { Button } from '@/components/ui/Button.tsx';
+import { Input } from '@/components/ui/Input.tsx';
 
 export const Login = () => {
   return (
--- src/pages/Auth/Onboarding/index.tsx
+++ src/pages/Auth/Onboarding/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router';
 
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/Button.tsx';
 import { RoutesEnum } from '@/enums/router';
 
 export const Onboarding = () => {
