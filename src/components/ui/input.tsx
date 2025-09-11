2b7378e283432a9927e38d67b3f9f4fcf600b016
lowfnm <artem.melinevskiy21@gmail.com>
2025-09-11 06:48:39 +0300

    fix: import

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import type { ComponentProps } from 'react';
 
 import { cn } from '@/lib/utils';
 
-export const Input = ({ className, type, ...props }: React.ComponentProps<'input'>) => {
+export const Input = ({ className, type, ...props }: ComponentProps<'input'>) => {
   return (
     <input
       type={type}
