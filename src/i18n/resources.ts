93972132a7e80a0f4e7bc8c673fb03eb0e1ca1de
lowfnm <artem.melinevskiy21@gmail.com>
2025-09-12 06:07:14 +0300

    feat: add translations to existing pages

---
+++ src/i18n/resources.ts
@@ -0,0 +1,11 @@
+import authEn from './locales/en/auth.json';
+import authFr from './locales/fr/auth.json';
+
+export const resources = {
+  fr: {
+    auth: authFr,
+  },
+  en: {
+    auth: authEn,
+  },
+};
